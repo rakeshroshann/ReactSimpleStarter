225aaac6e03fd7d5f26e93f485afe8cedcb6890b
Rakesh Nayak <Rakesh.Nayak@in.tesco.com>
2016-01-21 06:51:20 +0530

    Youtube Search key and npm added

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Component } from 'react';
+import SearchBar from './search_bar';
+import YTSearch from 'youtube-api-search';
 
+const API_KEY='AIzaSyBRIz5D_uKMo_usA98jJKQcfxOLCvaDQ1E';
+YTSearch({key: API_KEY,term: 'surfboards'}, (data) => {
+	console.log(data);
+});
 export default class App extends Component {
   render() {
     return (
-      <div>Hello!! Welcome to React simple starter</div>
+    	<div>
+      	<SearchBar/>
+      </div>
     );
   }
 }
