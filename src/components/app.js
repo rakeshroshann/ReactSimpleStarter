1b09ecd760c012422a722adebbb67e3f59d8bf5e
Rakesh Nayak <Rakesh.Nayak@in.tesco.com>
2016-01-21 07:15:05 +0530

    added video_list and item

--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,23 @@
 import React, {Component} from 'react';
 import SearchBar from './search_bar';
 import YTSearch from 'youtube-api-search';
+import VideoList from './video_list';
 
 const API_KEY='AIzaSyBRIz5D_uKMo_usA98jJKQcfxOLCvaDQ1E';
-YTSearch({key: API_KEY,term: 'surfboards'}, (data) => {
-	console.log(data);
-});
+
 export default class App extends Component {
+	constructor(props){
+		super(props);
+		this.state={videos: [] };
+		YTSearch({key: API_KEY,term: 'surfboards'}, (data) => {
+			this.setState({videos:data});
+		});
+	}
   render() {
     return (
     	<div>
       	<SearchBar/>
+      	<VideoList videos={this.state.videos}/>
       </div>
     );
   }
