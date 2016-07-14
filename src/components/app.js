7de03c66355262c260051f95220858db77e1c4c8
Rakesh Nayak <rakesh.nayak@in.tesco.com>
2016-07-14 20:32:12 +0100

    Reafactor

--- src/components/app.js
+++ src/components/app.js
@@ -4,34 +4,39 @@ import YTSearch from 'youtube-api-search';
 import VideoList from './video_list';
 import VideoDetail from './video_detail';
 import _ from 'lodash';
-const API_KEY='AIzaSyBRIz5D_uKMo_usA98jJKQcfxOLCvaDQ1E';
+const API_KEY = 'AIzaSyBRIz5D_uKMo_usA98jJKQcfxOLCvaDQ1E';
 
 export default class App extends Component {
-	constructor(props){
-		super(props);
-		this.state={videos: [], currentSelectedVideo: null };
-		this.videoSearch('surfboards');
-	}
-  videoSearch(term){
-    YTSearch({key: API_KEY,term: term}, (data) => {
+  constructor(props) {
+    super(props);
+    this.state = {videos: [], currentSelectedVideo: null};
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (data) => {
       this.setState({
-      	videos: data,
-      	currentSelectedVideo: data[0]
+        videos: data,
+        currentSelectedVideo: data[0]
       });
     });
   }
-  selectVideo(video){
-  	  this.setState({
-      	currentSelectedVideo: video
-      });
+
+  selectVideo(video) {
+    this.setState({
+      currentSelectedVideo: video
+    });
   }
+
   render() {
-  	const videoSearch = _.debounce((term)=> {this.videoSearch(term)}, 300);
+    const videoSearch = _.debounce((term)=> {
+      this.videoSearch(term)
+    }, 300);
     return (
-    	<div>
-      	<SearchBar onSearchTermChange={videoSearch}/>
-      	<VideoDetail video ={this.state.currentSelectedVideo}/>
-      	<VideoList videos={this.state.videos} onVideoSelect={video=> this.selectVideo(video)} />
+      <div>
+        <SearchBar onSearchTermChange={videoSearch}/>
+        <VideoDetail video={this.state.currentSelectedVideo}/>
+        <VideoList videos={this.state.videos} onVideoSelect={video=> this.selectVideo(video)}/>
       </div>
     );
   }
--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
 import VideoListItem from './video_list_item';
 export default class VideoList extends Component{
 	constructor(props){
 		super(props);
 	}
 	render(){
-		const videoListItems = this.props.videos.map((video) => <VideoListItem key={video.etag} video={video} onVideoSelect={this.props.onVideoSelect} />);
+		const videoListItems = this.props.videos.map((video) =>
+			<VideoListItem 
+          key={video.etag} 
+          video={video} 
+          onVideoSelect={this.props.onVideoSelect} />);
 		return (
 			<ul className='col-md-4 list-group'>
 				{videoListItems}
